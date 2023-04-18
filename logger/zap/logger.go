0f3aeea1804aba41a6a8e99bbbbe0f50f024ed4f
jacexh <jace.xh@gmail.com>
2023-04-18 09:51:36 +0000

    update

--- logger/zap/logger.go
+++ logger/zap/logger.go
@@ -20,7 +20,7 @@ func NewLog(zl *zap.Logger) logger.Logger {
 }
 
 func (zl *ZapLogger) Log(level logger.Level, kvs ...interface{}) {
-	if len(kvs) == 0 || len(kvs)%2 != 0 {
+	if len(kvs) == 0 || len(kvs)&1 != 0 {
 		zl.logger.Warn("", zap.Error(fmt.Errorf("kvs must appear in paris: %v", kvs)))
 		return
 	}
